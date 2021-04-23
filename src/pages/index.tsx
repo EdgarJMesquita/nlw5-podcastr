ca56be946dc9fc5520cefde8efbb233019cc2255
EdgarXongas <73535889+EdgarXongas@users.noreply.github.com>
2021-04-23 08:14:30 -0300

    primeiroCommit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,112 @@
 
-export default function Home(){
+import { GetStaticProps } from 'next'
+import Image from 'next/image'
+import { api } from '../services/api'
+import { format, parseISO } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+import convertDurationToTimeString from '../utils/convertDurationToTimeString'
+
+import styles from './home.module.scss'
+
+type Episode = {
+  id: string;
+  title: string;
+  description: string;
+  members: string;
+  duration: string;
+  thumbnail: string;
+  durationAsString: string;
+  url: string;
+  publishedAt: string;
+}
+
+type HomeProps = {
+  latestEpisodes: Episode[];
+  allEpisodes: Episode[];
+}
+
+export default function Home({ latestEpisodes ,allEpisodes }: HomeProps){
   return(
-    <div>
-      <h1>Hello World</h1>
+    <div className={styles.homePage}>
+     <section className={styles.latestEpisodes}> 
+      <h2>Últimos lançamentos</h2>
+
+      <ul>
+        {latestEpisodes.map(episode=>{
+          return (
+            <li key={episode.id}>
+              <Image 
+                width={192} 
+                height={192} 
+                src={episode.thumbnail} 
+                alt={episode.title} 
+                objectFit="cover"
+              />
+
+              <div className={styles.episodeDetails}>
+                <a href={episode.url}>{episode.title}</a>
+                <p>{episode.members}</p>
+                <span>{episode.publishedAt}</span>
+                <span>{episode.durationAsString}</span>
+              </div>
+
+              <button type="button">
+                <img src="/play-green.svg" alt="Tocar Episode"/>
+              </button>
+            </li>
+          )
+        })}
+      </ul>
+     </section>
+
+     <section className={styles.allEpisodes}> 
+        <h2>Todos episódios</h2>
+        <table cellSpacing={0}>
+          <thead>
+            <th></th>
+            <th>Podcast</th>
+            <th>Integrantes</th>
+            <th>Data</th>
+            <th>Duração</th>
+          </thead>
+        </table>
+     </section>
     </div>
   )
+}
+
+export const getStaticProps: GetStaticProps = async ()=> {
+
+  const { data }= await api.get('episodes',{
+    params:{
+      _limite: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+
+  const episodes = data.map(episode=>{
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at),'d MMM yy', { locale: ptBR }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url,
+    }
+  })
+  const latestEpisodes = episodes.slice(0, 2)
+  const allEpisodes = episodes.slice(2, episodes.lenght)
+  console.log(episodes[0].publishedAT)
+
+  return{
+    props:{
+      latestEpisodes,
+      allEpisodes
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
---
+++ src/utils/convertDurationToTimeString.tsx
@@ -0,0 +1,14 @@
+
+export default function convertDurationToTimeString(duration: number) {
+
+    const hours = Math.floor(duration/3600)
+    const minutes = Math.floor(duration % 3600 / 60)
+    const seconds = Math.floor(duration / 60)
+
+    let timeString = [hours,minutes,seconds]
+    .map(unit=>{
+       return String(unit).padStart(2,'0')
+    }).join(':')
+
+    return timeString
+}
